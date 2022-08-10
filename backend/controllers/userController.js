dd2f9e01d0ceaa41c781b9faf9c5ff2124985c2d
jungsikjeong <wndtlr1024@gmail.com>
2022-08-10 11:54:30 +0900

    proxy Error

---
+++ backend/controllers/userController.js
@@ -0,0 +1,103 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/userModel');
+
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
+  // 사용자가 이미 존재하는지 확인
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new error('Invalid user data');
+  }
+};
+
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // Check user and password match
+  // 바로 조건문으로 사용하고자해서 await를 사용해준것 같다.
+  // 다른 변수에 user.password를 대입시키고 if문에 그 변수를 넣어주면 await를 사용안해도된다.
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
+
+  res.send('Register Route');
+});
+
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  // authMiddleware에서 req.user에 id를 담아서 주고있기때문에
+  // req.user를 참조할 수 있다.
+  //  req.user = await User.findById(decoded.id).select('-password');
+  res.status(200).json(user);
+});
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('Server...');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// .json()은 JSON형태의 데이터를 해석해줌
+// .urlencoded()은 x-www-form-urlencoded형태의 데이터를 해석,
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
